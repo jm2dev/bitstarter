4c715e74815eb1118a15f183e272c7be7a0bb49a
jm2dev <jm2dev@gmail.com>
2013-07-10 23:34:42 +0000

    Using method buf.toString().

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var index = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(index.toString);
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
